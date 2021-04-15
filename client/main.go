4bf5920b50dd52e2d27446ab19fb7c2ded5f66d0
kapibara824 <kapibara824.dev@gmail.com>
2021-04-15 09:25:17 +0900

    first commit

---
+++ client/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"net"
+)
+
+func main() {
+
+	SendPacket()
+
+}
+
+func SendPacket() {
+	sum := 0
+	ec := 0
+	count := 0
+	data := 0
+	repeat := bytes.Repeat([]byte("a"), 2048*10)
+	//res := make([]byte, 1024)
+	//b := []byte("hello")
+	//buf := make([]byte, 1024)
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8240")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+
+		n, err := conn.Write(repeat)
+		if err == nil {
+			data = int(n)
+		}
+		count++
+		sum += int(n)
+		if err != nil {
+			ec++
+			if ec > 5 {
+				log.Println(count, data, count*data, ec)
+				log.Println("send", sum, "bytes")
+				log.Fatal("Error Count is 5")
+			}
+		}
+		/*
+			r, err := conn.Read(res)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(r)
+		*/
+	}
+
+}
---
+++ server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var sum int64 = 0
+var tsum int64 = 0
+
+func main() {
+	CloseHandler()
+
+	addr, _ := net.ResolveTCPAddr("tcp", ":8240")
+	conn, _ := net.ListenTCP("tcp", addr)
+
+	fmt.Println("Listening...")
+	defer conn.Close()
+
+	n, _ := conn.AcceptTCP()
+	defer n.Close()
+
+	ticker := time.NewTicker(time.Second * 3)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("%.3fGbps\n", float64(sum-tsum)/3/1024/1024/1024*8)
+			tsum = sum
+		default:
+			n, _ := io.CopyN(io.Discard, n, 1024*1024)
+			sum += n
+		}
+	}
+}
+
+func CloseHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		log.Println("interrupted")
+		log.Println("received", sum, "bytes")
+		os.Exit(0)
+	}()
+}
