116a9e6938fa5f5776373ce46e8885abba622f04
kapibara824 <hiroki.dev0824@gmail.com>
2021-04-16 17:28:30 +0900

    add time option

--- Perf/StartTcpServer.go
+++ Perf/StartTcpServer.go
@@ -8,13 +8,12 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 var sum int64 = 0
 var tsum int64 = 0
 
-func StartTcpServer() {
+func StartTcpServer(time int) {
 	CloseHandler()
 
 	addr, _ := net.ResolveTCPAddr("tcp", ":8240")
@@ -34,9 +33,13 @@ func StartTcpServer() {
 		sum += n
 		select {
 		case <-ticker.C:
-
 			log.Printf("%.3fGbps\n", float64(sum-tsum)/3/1024/1024/1024*8)
 			tsum = sum
+		case <-time.After(time * time.Second):
+			total := int(sum)
+			fmt.Println("%.3Gbps\n", total/time/1024/1024/1024*8)
+			fmt.Println("Received", total, "Gbytes")
+			os.Exit(0)
 		default:
 		}
 	}
@@ -48,7 +51,7 @@ func CloseHandler() {
 	go func() {
 		<-c
 		log.Println("interrupted")
-		log.Println("received", sum, "bytes")
+		log.Println("received", sum/1024/1024/1024, "Gbytes")
 		os.Exit(0)
 	}()
 }
