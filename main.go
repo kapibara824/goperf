9d51321b6083c5d1ae88fad7282b842f3a1ce298
kapibara824 <hiroki.dev0824@gmail.com>
2021-04-16 18:15:38 +0900

    modify flag

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 var (
 	servermode = flag.Bool("s", false, "Server mode")
 	ipaddr     = flag.String("c", "localhost", "Destination Address")
-	time       = flag.Int("t", 10, "time")
+	duration   = flag.String("t", "10", "time")
 )
 
 func main() {
 	flag.Parse()
 	if *servermode {
 		log.Println("Running Server mode.")
-		Perf.StartTcpServer(*time)
+		Perf.StartTcpServer(*duration)
 	}
 	if !*servermode {
 		log.Println("Running Client mode.")
